7ffc664f5143b7576f66ad9659ed445fb95a7612
Givailson de Souza Neves <givailson@Givailsons-MacBook-Pro.local>
2022-03-30 10:04:27 -0300

    footer

--- app/src/components/templates/Footer/view.tsx
+++ app/src/components/templates/Footer/view.tsx
@@ -1,14 +1,12 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
-import logo from "../,,/../../../assets/images/footer_logo.png";
-import { SocialLinks } from "../../../store/models/social-links";
-import SocialLinksAtom from "../../atoms/social-links";
+import logo from "../,,/../../../assets/images/2_logo.png";
 
 import "./styles.scss";
 
 export interface FooterProps {
-  socialLinks: SocialLinks;
+  socialLinks: any;
 }
 
 const Footer: React.FC<FooterProps> = ({ socialLinks }) => {
@@ -16,19 +14,40 @@ const Footer: React.FC<FooterProps> = ({ socialLinks }) => {
 
   return (
     <footer>
-      <Container fluid>
-        <Row className="footerInfo">
-          <Col md={{ offset: 4, span: 4 }} sm={12}>
-            <div>
-              <img src={logo} alt={t("app.title")} />
-            </div>
-          </Col>
-          <Col sm={12} md={4}>
-            <div className="social-links-area">
-              <SocialLinksAtom links={socialLinks} />
-            </div>
-          </Col>
-        </Row>
+      <Container>
+        <div className="call">
+          <p>
+            Este site é operado, mantido e de inteira propriedade da SouCred
+            Ltda., inscrita no CNPJ n° 39.581.101/0001-39, sediada em
+            Brasília-DF, SHIS QI 21, comércio local, bloco A, sala 204 – Lago
+            Sul, e está hospedado com o endereço www.eusoucred.com.br e suas
+            extensões. Atendimento disponível de segunda a sexta das 8h às 18h
+            pelo telefone (61) 99651 7829. Somos um correspondente bancário,
+            atividade regulada pelo Banco Central do Brasil, das seguintes
+            instituições financeiras:
+          </p>
+          <p>
+            Crédito pessoal e consignado: As taxas de juros e prazos praticados
+            nessas modalidades seguem as determinações particulares de cada
+            convênio, assim como os termos e políticas vigentes no ato da
+            contratação. Todos os valores e taxas mencionados tratam-se de
+            simulações, podendo variar no momento da contratação do crédito.
+            Antecipação saque-aniversário FGTS: Trata-se da antecipação de
+            recursos financeiros utilizando o saldo do FGTS como garantia. As
+            parcelas serão debitadas anualmente, no mês em que seria efetuado o
+            saque aniversário, diretamente na conta vinculada ao FGTS. Todos os
+            valores e taxas mencionados tratam-se de simulações, podendo variar
+            no momento da contratação do crédito.
+          </p>
+        </div>
+        <div className="links">
+          <div>
+            <a href="#">Política de privacidade</a>
+            <span></span>
+            <a href="#">Termos de uso</a>
+          </div>
+          <img src={logo} alt={t("general.appName")} />
+        </div>
       </Container>
     </footer>
   );
