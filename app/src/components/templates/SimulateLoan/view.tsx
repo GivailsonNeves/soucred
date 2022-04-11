05add9584e73bd78796c3bede7f9f34b0b8148ba
Givailson <vandopintoo@gmail.com>
2022-04-11 18:52:42 -0300

    tela

--- app/src/components/templates/SimulateLoan/view.tsx
+++ app/src/components/templates/SimulateLoan/view.tsx
@@ -1,9 +1,11 @@
 import { useTranslation } from "react-i18next";
-import { Container } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 import SimulatorPanel from "../../molecules/simulator-panel";
+import SignUpMolecule from "../../molecules/sign-up-molecule/view";
+import MoneyField from "../../atoms/money-field";
 
 import "./styles.scss";
-import SignUpMolecule from "../../molecules/sign-up-molecule/view";
+import celphone from "../,,/../../../assets/images/maopequena.png";
 
 const SimulateLoan: React.FC = () => {
   const [t] = useTranslation();
@@ -19,34 +21,93 @@ const SimulateLoan: React.FC = () => {
             <p>√ Melhores taxas do mercado.</p>
             <p>√ Dinheiro na conta em até 48h.</p>
           </div>
+        </div>
+        <div className="firt-tab">
+          <div className="group-btn">
+            <div className="button-marked"><p>1</p> Simulador</div>
+            <div className="button"><p>2</p>Cadastro</div>
+            <div className="button"><p>3</p>Resultado</div>
+          </div>
+          <div className="cardSimulator">
+            <SimulatorPanel
+              title="De quanto você precisa?"
+              taxValue={2.14}
+            />
+          </div>
+          <SignUpMolecule
+            numInstallments={[80, 70, 60]}
+          ></SignUpMolecule>
+          <div className="base-sub-title">
+            <p>Essas parcelas podem ser ainda melhores.</p>
+            <p>Envie seus documentos e faça sua simulação personalizada.</p>
+          </div>
+        </div>
 
+        <div className="second-tab">
+          <div className="group-btn">
+            <div className="button-marked"><p>1</p> Simulador</div>
+            <div className="button"><p>2</p>Cadastro</div>
+            <div className="button"><p>3</p>Resultado</div>
+          </div>
+          <Form className="form-data">
+            <Form.Group className="input-field input-field-one sm" controlId="name">
+              <Form.Label>Seu nome:</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group className="input-field input-field-two sm" controlId="whatapp">
+              <Form.Label>Seu WhatsApp:</Form.Label>
+              <Form.Control type="tel" />
+            </Form.Group>
+            <Form.Group className="input-field input-field-three sm" controlId="email">
+              <Form.Label>Seu e-mail:</Form.Label>
+              <Form.Control type="email" />
+            </Form.Group>
+            <Form.Group className="input-field input-field-four sm" controlId="email">
+              <Form.Label>Qual o valor desejado?</Form.Label>
+              <MoneyField></MoneyField>
+            </Form.Group>
+            <Form.Group className="input-field input-field-five sm" controlId="email">
+              <Form.Label>Em quantas parcelas deseja pagar?</Form.Label>
+              <MoneyField></MoneyField>
+            </Form.Group>
+            <Form.Group className="input-field input-field-six sm" controlId="email">
+              <Form.Label>Comprovante de renda</Form.Label>
+              <Button>ANEXAR</Button>
+            </Form.Group>
+            <Form.Group className="input-field input-field-seven sm" controlId="email">
+              <Form.Label>Comprovante de residência</Form.Label>
+              <Button>ANEXAR</Button>
+            </Form.Group>
+            <Form.Group className="input-field input-field-eight sm" controlId="email">
+              <Form.Label>Comprovante de residência</Form.Label>
+              <Button>ANEXAR</Button>
+            </Form.Group>
+            <div className="send-container">
+              <Button type="submit" className="send">
+                ENVIAR
+              </Button>
+            </div>
+          </Form>
         </div>
 
 
+        <div className="third-tab">
+          <div className="group-btn">
+            <div className="button-marked"><p>1</p> Simulador</div>
+            <div className="button"><p>2</p>Cadastro</div>
+            <div className="button"><p>3</p>Resultado</div>
+          </div>
 
-        <div className="group-btn">
-          <div className="button"><p>1</p> Simulador</div>
-          <div className="button"><p>2</p>Cadastro</div>
-          <div className="button"><p>3</p>Resultado</div>
+
+          <div className="container">
+            <img src={celphone} />
+            <div className="tanks">
+              <p>Obrigado por enviar seus documentos! Entraremos em contato muito em breve via WhatsApp.</p>
+            </div>
+          </div>
         </div>
 
 
-
-        <div className="cardSimulator">
-          <SimulatorPanel
-            title="De quanto você precisa?"
-            taxValue={2.14}
-          />
-        </div>
-
-        <SignUpMolecule
-          numInstallments={[80, 70, 60]}
-        ></SignUpMolecule>
-
-        <div className="base-sub-title">
-          <p>Essas parcelas podem ser ainda melhores.</p>
-          <p>Envie seus documentos e faça sua simulação personalizada.</p>
-        </div>
       </Container>
     </section>
   );
